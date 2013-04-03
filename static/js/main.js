9dac4ac5e76a5225f561eaf6750875a7cde3f0b2
NC12 <ng.cheryl@dhs.sg>
2013-04-03 11:52:34 +0800

    edited comments

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 	$('#content').fadeIn('slow');
+    // gets json data
     $.getJSON('feed.json', function(data) {
+   	// loops through data
        $.each(data, function(key, value) {
+       	// writes json data to html
           var news = "<section>" +
 		  "<a href=" + value.link + ">" + value.title + "</a>" +
 		  "<p>" + value.description + "</p>" +
